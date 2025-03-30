8c4857ed338040eb535f380808490c1d20d73407
skalmanGPK <pineappel92@gmail.com>
2025-03-30 14:43:25 +0200

    updated the app.py a bit

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, send_from_directory, render_template, redirect, url_for
+from flask import Flask, request, send_from_directory, render_template, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
 import os
+
+from models import ActivityLog, User
 
 app = Flask(__name__, template_folder='templates') # Initializes the Flask application
 
@@ -58,10 +61,35 @@ def login():
         user = User.query.filter_by(username=username).first()
 
         if user and check_password_hash(user.password_hash, password):
-            return "Login successful", 200
+            session['user_id'] = user.id # Store user in session
+            return redirect(url_for('home'))
         else:
             return "Invalid credentials", 401
     return render_template("login.html")
+@app.route("/logout")
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('home'))
+@app.route("/reigster", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        email = request.form.get("email", "")
 
+        # Check if username exists
+        if User.query.filter_by(username=username).first():
+            return  "Username already exists", 400
+        
+        # Create new user
+        new_user = User(
+            username=username,
+            password_hash=generate_password_hash(password)
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+    return render_template("register.html")
 if __name__ == "__main__":
     app.run(debug=True) # Starts the Flask development server with debugging enabled
